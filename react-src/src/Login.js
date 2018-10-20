49b8a9d140854364d603f8adab46e23f21fd3351
Francesco Lusetti <0002luso.ciccio@gmail.com>
2018-10-20 22:47:14 +0200

    Bettering login and register

--- react-src/src/Login.js
+++ react-src/src/Login.js
@@ -23,11 +23,11 @@ export default class Login extends Component{
       <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
         <ModalHeader toggle={this.toggle}>Login I GRUPPI</ModalHeader>
         <ModalBody>
-          <Input type="username" id="username" name="username" placeholder="EMail" />
+          <Input type="email" id="username" name="username" placeholder="EMail" />
           <Input type="password" id="password" name="password" placeholder="Password" />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={props.LoginHandler}>Login</Button>
+          <Button color="primary" onClick={this.props.LoginHandler}>Login</Button>
           <Button color="secondary" onClick={this.toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
--- react-src/src/Register.js
+++ react-src/src/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { InputGroup, InputGroupText, InputGroupAddon, Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 
 export default class Register extends Component{
     constructor(props) {
@@ -20,14 +20,25 @@ export default class Register extends Component{
     render(props){
       <div>
       <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-      <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-        <ModalHeader toggle={this.toggle}>Login I GRUPPI</ModalHeader>
+      <Modal isOpen={this.state.modal} toggle={this.toggle}>
+        <ModalHeader toggle={this.toggle}>Registrati ne I GRUPPI</ModalHeader>
         <ModalBody>
-          <Input type="username" id="username" name="username" placeholder="EMail" />
+          <Input type="text" id="firstname" name="firstname" placeholder="Nome" />
+          <Input type="text" id="surname" name="surname" placeholder="Cognome" />
+          //Bisogna mettere il fatto che la provincia é formata da solo 2 lettere e deve essere controllata
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>Via</InputGroupText>
+            </InputGroupAddon>
+            <Input type="text" id="indirizzo" name="indirizzo" placeholder="Indirizzo" />
+          </InputGroup>
+          <Input type="text" id="localita" name="localita" placeholder="Localitá" />
+          <Input type="text" id="provincia" name="provincia" placeholder="Provincia" />
+          <Input type="email" id="username" name="username" placeholder="EMail" />
           <Input type="password" id="password" name="password" placeholder="Password" />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={props.RegisterHandler}>Login</Button>
+          <Button color="primary" onClick={this.props.RegisterHandler}>Login</Button>
           <Button color="secondary" onClick={this.toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>

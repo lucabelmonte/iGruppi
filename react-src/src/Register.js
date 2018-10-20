609232f0547a00b8e1a36cc98283b0da49806f32
Luso13 <francesco.lusetti13@gmail.com>
2018-10-20 22:11:34 +0200

    Register added

---
+++ react-src/src/Register.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+export default class Register extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        };
+    
+        this.toggle = this.toggle.bind(this);
+      }
+
+      toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
+
+    render(props){
+      <div>
+      <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+      <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+        <ModalHeader toggle={this.toggle}>Login I GRUPPI</ModalHeader>
+        <ModalBody>
+          <Input type="username" id="username" name="username" placeholder="EMail" />
+          <Input type="password" id="password" name="password" placeholder="Password" />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={props.RegisterHandler}>Login</Button>
+          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+    }
+};
+

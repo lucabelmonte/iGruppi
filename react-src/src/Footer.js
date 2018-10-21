988b655027fb7b442f29ae5bda40c286fe91aad0
Francesco Lusetti <0002luso.ciccio@gmail.com>
2018-10-21 09:39:09 +0200

    Footer pt 2

--- react-src/src/Footer.js
+++ react-src/src/Footer.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
-import { InputGroup, InputGroupText, InputGroupAddon, Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import './Footer.css';
+import {
+    Col,
+    Row,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink } from 'reactstrap';
 
 export default class Footer extends Component{
+    constructor(props) {
+        super(props);
+      }
     render(){
-        return null;
+        return (
+            <div className="Footer bg-dark">
+                <Row>
+                    <Col xs="3">
+                            <NavLink href="https://github.com/iGruppi/iGruppi">GitHub</NavLink>
+                    </Col>
+                    <Col xs="6">
+                            <NavbarBrand className="text-white" href="#">I GRUPPI</NavbarBrand>
+                    </Col>
+                    <Col xs="3">
+                    <NavbarBrand className="text-white">I GRUPPI</NavbarBrand>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-white">GPL License I GRUPPI 2018</Col>
+                </Row>
+            </div>
+        )
     }
 }
